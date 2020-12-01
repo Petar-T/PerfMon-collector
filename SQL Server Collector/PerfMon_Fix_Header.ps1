01356991a5c819e49afbedd262e86c8ce2acc738
Petar Trbos <47648550+Petar-T@users.noreply.github.com>
2020-12-01 23:17:00 +0100

    Update PerfMon_Fix_Header.ps1

--- SQL Server Collector/PerfMon_Fix_Header.ps1
+++ SQL Server Collector/PerfMon_Fix_Header.ps1
@@ -1,7 +1,10 @@
-﻿$FileName =$PSScriptRoot + "\SQLPerfMon-Server_201912021442.csv"
-$NewFileName =$PSScriptRoot + "\PerfMon_SourceData.csv"
-$BackupFileName =$PSScriptRoot + "\TsvCopy.zip"
+$fldr=$PSScriptRoot
 $FirsColValue="Date_Time"
+$DefInst="SQLServer"
+
+$FileName =$fldr + "\SQLPerfMon-Server_201912021442.csv"
+$NewFileName =$fldr + "\PerfMon_SourceData.csv"
+$BackupFileName =$fldr + "\TsvCopy.zip"
 
 
 if (-not (test-path $BackupFileName))
@@ -9,19 +12,27 @@ if (-not (test-path $BackupFileName))
 else
   {$opts = @{'Path' = $FileName; 'DestinationPath' = $BackupFileName; 'Confirm'=$true ; 'Update'=$true  }}
 
-
  Compress-Archive @opts
-
 
 $HeaderRow = Get-Content $FileName -First 1
 $Split1=$HeaderRow|%{$_.split('"')[1]}
 $Split2=$HeaderRow|%{$_.split('"')[3]}
 $Split3=$Split2|%{$_.split('\\')[2]}
+$Split4=$Split2.Substring($Split3.Length+3).Split(':')[0]
 
-#$Split1
 
-
-#$NewFile=(Get-Content $FileName).Replace( $Split1 , '(PDH-TSV 4.0) (Coordinated Universal Time)(0)').Replace($Split3, 'SQL2019') 
+Write-Host  'Changing column name' [$Split1] 'to' [$FirsColValue] -ForegroundColor Green
 $NewFile=(Get-Content $FileName).Replace( $Split1 , $FirsColValue)
-#Set-Content -Path $FileName -Value  $NewFile -Force
+
+if ($Split4 -ne $DefInst)
+{
+    $NewFile=$NewFile.Replace( $Split4 , $DefInst)
+    Write-Host 'Named instance found: ' $Split4 -ForegroundColor Green
+    Write-Host  'standardizing column name' $Split2 -ForegroundColor Green
+}
+else
+{
+    Write-Host 'Default instance found: ' $Split4 -ForegroundColor Green
+}
+
 Set-Content -Path $NewFileName -Value  $NewFile -Force
